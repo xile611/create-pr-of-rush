aef321d62d36e84692655f8a0c1a5456d14549db
xile611 <dingling112@gmail.com>
2023-06-16 14:51:46 +0800

    build: update dist

--- dist/index.js
+++ dist/index.js
@@ -64,16 +64,42 @@ run();
 /***/ }),
 
 /***/ 385:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.convertLogsToMarkdown = exports.readChangelogOfVersion = exports.getPathOfPackages = exports.findJsonFile = void 0;
+const core = __importStar(__nccwpck_require__(186));
 const path_1 = __nccwpck_require__(17);
 const fs_1 = __nccwpck_require__(147);
 const findJsonFile = (path, filename) => {
     try {
+        const filePath = (0, path_1.join)(path, filename);
+        core.info(`[info] read json file: ${filePath}`);
         return JSON.parse((0, fs_1.readFileSync)((0, path_1.join)(path, filename)).toString());
     }
     catch (e) {
@@ -103,6 +129,9 @@ const readChangelogOfVersion = (version, rushPath, filterTags, blackList) => {
     const logs = [];
     const changlogInfos = (0, exports.getPathOfPackages)(rushPath, filterTags, blackList);
     if (changlogInfos && changlogInfos.length) {
+        core.info(`[info] read json file: \n ${changlogInfos
+            .map(entry => `- ${entry.path}/${entry.fileName}`)
+            .join('\n')}`);
         // eslint-disable-next-line github/array-foreach
         changlogInfos.forEach(entry => {
             var _a, _b, _c, _d, _e, _f;
